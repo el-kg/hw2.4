b15aabf59119ffcac1071f92f26d2447a3e11072
el-kg <kirillgalkin8@gmail.com>
2023-07-19 22:39:30 +0700

    Внесены правки: -добавлены файлы application.properties и pom.xml -@RequestMapping("/calculator") -добавлен условный оператор в метод divide -добавлен @Override

--- src/main/java/pro/sky/skyprospringcalculator/CalculatorControler.java
+++ src/main/java/pro/sky/skyprospringcalculator/CalculatorControler.java
@@ -1,10 +1,12 @@
 package pro.sky.skyprospringcalculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorControler {
     private final CalculatorService calculatorServiceImpl;
 
@@ -12,27 +14,27 @@ public class CalculatorControler {
         this.calculatorServiceImpl = calculatorServiceImpl;
     }
 
-    @GetMapping(path = "/calculator")
+    @GetMapping
     public String hello() {
         return calculatorServiceImpl.hello();
     }
 
-    @GetMapping(path = "/calculator/plus")
+    @GetMapping(path = "/plus")
     public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         return calculatorServiceImpl.plus(num1, num2);
     }
 
-    @GetMapping(path = "/calculator/minus")
+    @GetMapping(path = "/minus")
     public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         return calculatorServiceImpl.minus(num1, num2);
     }
 
-    @GetMapping(path = "/calculator/multiply")
+    @GetMapping(path = "/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         return calculatorServiceImpl.multiply(num1, num2);
     }
 
-    @GetMapping(path = "/calculator/divide")
+    @GetMapping(path = "/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         return calculatorServiceImpl.divide(num1, num2);
     }
--- src/main/java/pro/sky/skyprospringcalculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/skyprospringcalculator/CalculatorServiceImpl.java
@@ -1,32 +1,36 @@
 package pro.sky.skyprospringcalculator;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
+    @Override
     public String hello() {
         return "<b> Добро пожаловать в калькулятор! </b>";
     }
-
+    @Override
     public String plus(int num1, int num2) {
         int sum = num1 + num2;
         return num1 + " + " + num2 + " = " + sum;
     }
-
+    @Override
     public String minus(int num1, int num2) {
         int sum = num1 - num2;
         return num1 + " - " + num2 + " = " + sum;
     }
-
+    @Override
     public String multiply(int num1, int num2) {
         int sum = num1 * num2;
         return num1 + " * " + num2 + " = " + sum;
     }
-
+    @Override
     public String divide(int num1, int num2) {
         int sum = num1 / num2;
-        return num1 + " / " + num2 + " = " + sum;
+        if (num2 == 0) {
+            return " Делить на ноль нельзя!";
+        } else {
+            return num1 + " / " + num2 + " = " + sum;
+        }
     }
 }
